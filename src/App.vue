27aebac3073a25208ca319665b9347f6f6de1fa2
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-08-06 12:53:29 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { data } from "./data";
+const meal_data = ref([]);
+onMounted(() => {
+  meal_data.value = data;
+
+  // console.log(meal_data.value);
+});
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th scope="col">品項</th>
+        <th scope="col">描述</th>
+        <th scope="col">價格</th>
+        <th scope="col">庫存</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="m in meal_data" :key="m.name">
+        <td>{{ m.name }}</td>
+        <td>
+          <small>{{ m.description }}</small>
+        </td>
+        <td>{{ m.price }}</td>
+        <td><button>-</button>{{ m.count }}<button>+</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
